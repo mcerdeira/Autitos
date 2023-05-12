3a4e10edc741abed25a9bb16c08487254541a599
martin_cerdeira <Martin.Cerdeira@comafi.com.ar>
2023-05-12 10:34:05 -0300

    C

---
+++ scenes/carHUD.gd
@@ -0,0 +1,13 @@
+extends Node2D
+export var _CarObj: NodePath
+onready var CarObj : Node2D = get_node(_CarObj) as Node2D
+var player_num = ""
+
+func _ready():
+	$background.color = CarObj.color
+	player_num = CarObj.player_num
+
+
+func _physics_process(delta):
+	if is_instance_valid(CarObj):
+		$speed.text = str(round(CarObj.velocity.length()))
