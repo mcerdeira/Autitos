18643b70d90e60fafedab8df7690af0017da03b5
martin_cerdeira <Martin.Cerdeira@comafi.com.ar>
2023-05-12 17:19:32 -0300

    Music

--- scenes/Title.gd
+++ scenes/Title.gd
@@ -1,9 +1,6 @@
 extends Node2D
 var next = false
 
-func _ready():
-	Music.stop()
-
 func _input(event):
 	if event.is_action_pressed("acelerate_p1") or event.is_action_pressed("acelerate_p2") or event.is_action_pressed("acelerate_p3") or event.is_action_pressed("acelerate_p4"):
 		go_next(true)
@@ -15,8 +12,9 @@ func go_next(now = false):
 		if !now:
 			yield(get_tree().create_timer(5), "timeout")
 		get_tree().change_scene("res://scenes/PlayerSelect.tscn")
-
+		
 func _ready():
+	Music.stop()
 	$AnimationPlayer.play("New Anim")
 	for i in range(4):
 		yield(get_tree().create_timer(0.2), "timeout")
